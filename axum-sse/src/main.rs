f99a0565088b29724e94db6b797a673f94bdede2
Richard Hao <richard@0xdev.dev>
2023-06-25 14:39:39 +0800

    feat: add tracking aka logging

--- axum-sse/src/main.rs
+++ axum-sse/src/main.rs
@@ -1,9 +1,12 @@
 use std::{convert::Infallible, time::Duration};
 
 use axum::{
+    body::Bytes,
+    extract::MatchedPath,
+    http::{HeaderMap, Request},
     response::{
         sse::{Event, KeepAlive},
-        Sse,
+        Response, Sse,
     },
     routing::get,
     Router,
@@ -13,14 +16,68 @@ use futures_util::stream::{self, Stream};
 use serde_json::json;
 use tokio::signal;
 use tokio_stream::StreamExt;
+use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
+use tracing::{info_span, Span};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                "axum_sse=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new()
         .route("/", get(hello_handler))
-        .route("/time", get(current_time_handler));
+        .route("/time", get(current_time_handler))
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    // Log the matched route's path (with placeholders not filled in).
+                    // Use request.uri() or OriginalUri if you want the real path.
+                    let matched_path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
 
-    println!("Listening on 0.0.0.0:8080");
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        matched_path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+                .on_request(|request: &Request<_>, span: &Span| {
+                    // You can use `_span.record("some_other_field", value)` in one of these
+                    // closures to attach a value to the initially empty field in the info_span
+                    // created above.
+                    span.record("request path:", request.uri().path());
+                })
+                .on_response(|_response: &Response, _latency: Duration, _span: &Span| {
+                    // ...
+                })
+                .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {
+                    // ...
+                })
+                .on_eos(
+                    |_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {
+                        // ...
+                    },
+                )
+                .on_failure(
+                    |_error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
+                        // ...
+                    },
+                ),
+        );
+
+    tracing::info!("Listening on 0.0.0.0:8080");
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .with_graceful_shutdown(shutdown_signal())
@@ -61,6 +118,7 @@ async fn hello_handler() -> &'static str {
 async fn current_time_handler() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     let stream = stream::repeat_with(|| {
         let time = Utc::now().format("%y-%m-%d %H:%M:%S").to_string();
+        tracing::info!("Time: {}", time);
         let result = Event::default().json_data(json!({ "time": time }));
         match result {
             Ok(event) => event,
