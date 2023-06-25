c3c3a3244f61543f4f51f2f18963eb5b175ff85e
Richard Hao <richard@0xdev.dev>
2023-06-25 12:53:20 +0800

    feat: add graceful shutdown

--- axum-sse/src/main.rs
+++ axum-sse/src/main.rs
@@ -11,6 +11,7 @@ use axum::{
 pub(crate) use chrono::Utc;
 use futures_util::stream::{self, Stream};
 use serde_json::json;
+use tokio::signal;
 use tokio_stream::StreamExt;
 
 #[tokio::main]
@@ -22,8 +23,35 @@ async fn main() {
     println!("Listening on 0.0.0.0:8080");
     axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
 
 async fn hello_handler() -> &'static str {
