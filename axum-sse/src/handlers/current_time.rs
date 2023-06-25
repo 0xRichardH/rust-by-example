21c16d8a21e43287a5e42ec09065b370bb09c775
Richard Hao <richard@0xdev.dev>
2023-06-25 18:53:16 +0800

    refactor: use async_stream to rewrite the sse stream function

--- axum-sse/src/handlers/current_time.rs
+++ axum-sse/src/handlers/current_time.rs
@@ -1,26 +1,33 @@
 use std::{convert::Infallible, time::Duration};
 
+use async_stream::stream;
 use axum::response::{
     sse::{Event, KeepAlive},
     Sse,
 };
 use chrono::Utc;
-use futures_util::{stream, Stream};
+use futures_core::Stream;
 use serde_json::json;
-use tokio_stream::StreamExt;
 
 pub async fn current_time_handler() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
-    let stream = stream::repeat_with(|| {
-        let time = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
-        tracing::info!("Time: {}", time);
-        let result = Event::default().json_data(json!({ "time": time }));
-        match result {
-            Ok(event) => event,
-            Err(_) => Event::default().data("error"),
+    let s = stream! {
+        let mut interval = tokio::time::interval(Duration::from_secs(1));
+        loop {
+            tokio::select! {
+                _ = interval.tick() => {
+                    let now = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
+                    let result = Event::default().json_data(json!({ "time": now }));
+                    match result {
+                        Ok(e) => yield Ok(e),
+                        Err(err) => {
+                            tracing::error!("Error: {:?}", err);
+                            return;
+                        }
+                    }
+                }
+            }
         }
-    })
-    .map(Ok)
-    .throttle(Duration::from_secs(1));
+    };
 
-    Sse::new(stream).keep_alive(KeepAlive::default())
+    Sse::new(s).keep_alive(KeepAlive::default())
 }
