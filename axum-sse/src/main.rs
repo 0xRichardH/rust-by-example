c8530cc08fef2066b4b00a34d6d5dfd2429a7318
Richard Hao <richard@0xdev.dev>
2023-06-26 09:41:58 +0800

    refactor: refactor tracing subscriber initialization function
    - Move the `tracing_subscriber` initialization to a separate function.
    
    Signed-off-by: Richard Hao <richard@0xdev.dev>

--- axum-sse/src/main.rs
+++ axum-sse/src/main.rs
@@ -29,16 +29,7 @@ pub struct AppState {
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                // axum logs rejections from built-in extractors with the `axum::rejection`
-                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
-                "axum_sse=debug,tower_http=debug,axum::rejection=trace".into()
-            }),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    init_tracing_subscriber();
 
     let shared_state = AppState::default();
     let stream_handlers = Arc::clone(&shared_state.stream_handlers);
@@ -98,6 +89,19 @@ async fn main() {
         .with_graceful_shutdown(shutdown_signal(stream_handlers))
         .await
         .unwrap();
+}
+
+fn init_tracing_subscriber() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                "axum_sse=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 }
 
 async fn shutdown_signal(stream_handlers: Arc<Mutex<Vec<oneshot::Sender<()>>>>) {
